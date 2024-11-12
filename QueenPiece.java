0b69487db2b97e2e32af3d6e383f701df02e1816
kamal-hassan-1 <115949420+kamal-hassan-1@users.noreply.github.com>
2024-11-12 18:09:58 +0500

    Add files via upload

--- QueenPiece.java
+++ QueenPiece.java
@@ -7,14 +7,10 @@ public class QueenPiece extends ChessPiece {
     }
 
     ArrayList<ChessPosition> getAllPossibleMoves(ChessBoard board) {
-       
         RookPiece rook = new RookPiece(this.color, this.position);
         ArrayList<ChessPosition> moves = new ArrayList<>(rook.getAllPossibleMoves(board));
-        
         BishopPiece bishop = new BishopPiece(this.color, this.position);
         moves.addAll(bishop.getAllPossibleMoves(board));
-        
-        
         return moves;
     }
 }
