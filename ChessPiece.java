9c136c4cd9157f6da25bfda654e8eb553ee6dcb2
Abdul Ahad <46815089+n19h7mar3@users.noreply.github.com>
2024-11-12 17:48:26 +0500

    Add files via upload

--- ChessPiece.java
+++ ChessPiece.java
@@ -17,5 +17,25 @@ abstract class ChessPiece {
     char getColor() { return this.color; }
     String getPieceType() { return this.pieceType; }
 
+    boolean addMoveIfValidAndIndicateWhetherToContinue(ArrayList<ChessPosition> moves, ChessBoard board, int file, int rank) {
+        // Make sure proposed move is not out-of-bounds.
+        if (!ChessPosition.isValidPosition(file, rank)) {
+            return true; // Possibly okay to continue.
+        }
+
+        ChessPosition newPos = new ChessPosition(file, rank);
+
+        // Make sure any friendly piece is not at proposed position.
+        if (board.isAnyPieceOfSpecificColorAtPosition(newPos, this.color)) {
+            return false; // DO NOT CONTINUE.
+        }
+
+        // newPos is a valid move.
+        moves.add(newPos);
+        
+        // Return DO NOT CONTINUE (false) if any piece (of the other color) is in the way.
+        return !board.isAnyPieceOfSpecificColorAtPosition(newPos, Helpers.invertColor(this.color));
+    }
+
     abstract ArrayList<ChessPosition> getAllPossibleMoves(ChessBoard board);
 }
