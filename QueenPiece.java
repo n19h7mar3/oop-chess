a9815c2da5a298fa47124541f570b285b3465c9f
Raheel-ahm <164629808+Raheel-ahm@users.noreply.github.com>
2024-11-12 17:39:10 +0500

    Add files via upload

--- QueenPiece.java
+++ QueenPiece.java
@@ -4,17 +4,16 @@ public class QueenPiece extends ChessPiece {
 
     QueenPiece(char color, ChessPosition position)  {
         super(color, position);
-        this.pieceType = "queen";
     }
 
     ArrayList<ChessPosition> getAllPossibleMoves(ChessBoard board) {
         ArrayList<ChessPosition> moves = new ArrayList<>();
-
         RookPiece rook = new RookPiece(this.color, this.position);
+        moves.addAll(rook.getAllPossibleMoves(board));
         BishopPiece bishop = new BishopPiece(this.color, this.position);
-
-        moves.addAll(rook.getAllPossibleMoves(board), bishop.getAllPossibleMoves(board));
-
+        moves.addAll(bishop.getAllPossibleMoves(board));
+        
+        
         return moves;
     }
 }
