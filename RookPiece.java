9f9d6809389a3ab1c913bc7e5e40c2cddc0a78fb
Abdul Ahad <46815089+n19h7mar3@users.noreply.github.com>
2024-11-12 18:46:06 +0500

    Add files via upload

--- RookPiece.java
+++ RookPiece.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+
 public class RookPiece extends ChessPiece {
 
     RookPiece(char color, ChessPosition position)  {
@@ -6,102 +7,30 @@ public class RookPiece extends ChessPiece {
         this.pieceType = "rook";
     }
 
-    ArrayList<ChessPosition> getAllPossibleMoves(ChessBoard board){
+    ArrayList<ChessPosition> getAllPossibleMoves(ChessBoard board) {
         ArrayList<ChessPosition> moves = new ArrayList<>();
 
         int curFile = position.getFile();
         int curRank = position.getRank();
 
-        for(int r = curRank + 1; r <= 8; r++){
-
-            ChessPosition thisPos = new ChessPosition(curFile, r);
-
-            if(board.isAnyPieceOfSpecificColorAtPosition(thisPos,color)) break;
-
-            if(color == 'b' && board.isAnyPieceOfSpecificColorAtPosition(thisPos,'w')){
-                moves.add(thisPos);
-                break;
-            }else if(board.isAnyPieceOfSpecificColorAtPosition(thisPos,'b')){
-                moves.add(thisPos);
-                break;
-            }
-
-            if (ChessPosition.isValidPosition(thisPos.getFile(),thisPos.getRank()) && !thisPos.equals(position)) {
-
-                moves.add(thisPos);
-
-            }
+        // North Moves
+        for(int r = curRank + 1; r <= 8; r++) {
+            if (!addMoveIfValidAndIndicateWhetherToContinue(moves, board, curFile, r)) break;
         }
 
-        for(int r = curRank - 1; r >= 1; r--){
-
-            ChessPosition thisPos = new ChessPosition(curFile, r);
-
-            if(board.isAnyPieceOfSpecificColorAtPosition(thisPos,color)) break;
-            if(color == 'b' && board.isAnyPieceOfSpecificColorAtPosition(thisPos,'w')){
-                moves.add(thisPos);
-                break;
-            }else if(board.isAnyPieceOfSpecificColorAtPosition(thisPos,'b')){
-                moves.add(thisPos);
-                break;
-            }
-
-            if (ChessPosition.isValidPosition(thisPos.getFile(),thisPos.getRank()) && !thisPos.equals(position)) {
-
-                moves.add(thisPos);
-
-            }
+        // South Moves
+        for(int r = curRank - 1; r >= 1; r--) {
+            if (!addMoveIfValidAndIndicateWhetherToContinue(moves, board, curFile, r)) break;
         }
 
-        for(int f = curFile + 1; f <= 8; f++){
-
-            ChessPosition thisPos = new ChessPosition(f, curRank);
-
-//            if (board.isFriendlyPieceAtPostion(thisPos)) break;
-            if(board.isAnyPieceOfSpecificColorAtPosition(thisPos,color)) break;
-
-//            if (board.isEnemyPieceAtPosition(thisPos)){
-//                moves.add(thisPos);
-//                break;
-//            }
-            if(color == 'b' && board.isAnyPieceOfSpecificColorAtPosition(thisPos,'w')){
-                moves.add(thisPos);
-                break;
-            }else if(board.isAnyPieceOfSpecificColorAtPosition(thisPos,'b')){
-                moves.add(thisPos);
-                break;
-            }
-
-            if (ChessPosition.isValidPosition(thisPos.getFile(),thisPos.getRank()) && !thisPos.equals(position)) {
-
-                moves.add(thisPos);
-
-            }
+        // East Moves
+        for(int f = curFile + 1; f <= 8; f++) {
+            if (!addMoveIfValidAndIndicateWhetherToContinue(moves, board, f, curRank)) break;
         }
 
-        for(int f = curFile - 1; f >= 1; f--){
-
-            ChessPosition thisPos = new ChessPosition(f, curRank);
-
-//            if (board.isFriendlyPieceAtPostion(thisPos)) break;
-            if(board.isAnyPieceOfSpecificColorAtPosition(thisPos,color)) break;
-//            if (board.isEnemyPieceAtPosition(thisPos)){
-//                moves.add(thisPos);
-//                break;
-//            }
-            if(color == 'b' && board.isAnyPieceOfSpecificColorAtPosition(thisPos,'w')){
-                moves.add(thisPos);
-                break;
-            }else if(board.isAnyPieceOfSpecificColorAtPosition(thisPos,'b')){
-                moves.add(thisPos);
-                break;
-            }
-
-            if (ChessPosition.isValidPosition(thisPos.getFile(),thisPos.getRank()) && !thisPos.equals(position)) {
-
-                moves.add(thisPos);
-
-            }
+        // West Moves
+        for(int f = curFile - 1; f >= 1; f--) {
+            if (!addMoveIfValidAndIndicateWhetherToContinue(moves, board, f, curRank)) break;
         }
 
         return moves;
