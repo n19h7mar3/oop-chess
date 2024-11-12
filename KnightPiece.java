31340535870639b4b6ebb5854e952aed88293cf4
Abdul Ahad <46815089+n19h7mar3@users.noreply.github.com>
2024-11-12 18:21:36 +0500

    Add files via upload

--- KnightPiece.java
+++ KnightPiece.java
@@ -15,31 +15,18 @@ public class KnightPiece extends ChessPiece {
 
         int tempRank = curRank + 2;
         for(int i = 1; i <= 2; i++) {
-            addMoveIfValid(moves, board, curFile + i, tempRank);  // Top Right Move
-            addMoveIfValid(moves, board, curFile - i, tempRank);  // Top Left Move
+            addMoveIfValidAndIndicateWhetherToContinue(moves, board, curFile + i, tempRank);  // Top Right Move
+            addMoveIfValidAndIndicateWhetherToContinue(moves, board, curFile - i, tempRank);  // Top Left Move
             tempRank -= 1;
         }
 
         tempRank = curRank - 2;
         for(int i = 1; i <= 2; i++) {
-            addMoveIfValid(moves, board, curFile + i, tempRank);  // Bottom Right Move
-            addMoveIfValid(moves, board, curFile - i, tempRank);  // Bottom Left Move
+            addMoveIfValidAndIndicateWhetherToContinue(moves, board, curFile + i, tempRank);  // Bottom Right Move
+            addMoveIfValidAndIndicateWhetherToContinue(moves, board, curFile - i, tempRank);  // Bottom Left Move
             tempRank += 1;
         }
 
         return moves;
-    }
-
-    void addMoveIfValid(ArrayList<ChessPosition> moves, ChessBoard board, int file, int rank) {
-        // Make sure proposed move is not out-of-bounds
-        if (!ChessPosition.isValidPosition(file, rank)) return;
-
-        ChessPosition newPos = new ChessPosition(file, rank);
-
-        // Make sure any friendly piece is not at proposed position
-        if (board.isAnyPieceOfSpecificColorAtPosition(newPos, this.color)) return;
-
-        // newPos is a valid move
-        moves.add(newPos);
     }
 }
