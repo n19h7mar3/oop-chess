8db359b60acf7d116de2d8d4d8fa999c395def0b
Abdul Ahad <46815089+n19h7mar3@users.noreply.github.com>
2024-11-13 06:44:29 +0500

    Add files via upload

--- KingPiece.java
+++ KingPiece.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class KingPiece extends ChessPiece {
 
-    KingPiece(char color, ChessPosition position)  {
+    KingPiece(char color, ChessPosition position) {
         super(color, position);
         this.pieceType = "king";
     }
@@ -13,17 +13,17 @@ public class KingPiece extends ChessPiece {
         int curFile = position.getFile();
         int curRank = position.getRank();
 
-        for (int f= curFile-1; f <= curFile+1; f++){
-            for(int r = curRank -1 ; r <=curRank +1; r++){
-                ChessPosition thispos = new ChessPosition(f, r);
-                if(thispos.equals (position)){
-                    continue;
-                }
-                if (addMoveIfValidAndIndicateWhetherToContinue(moves, board, f, r)) break;
-                }
+        for (int f = curFile - 1; f <= curFile + 1; f++) {
+            for(int r = curRank - 1; r <= curRank + 1; r++) {
+
+                // Exclude our own position
+                if (position.equals(f, r)) continue;
+
+                // Add move to list; ignore continue-indicator
+                addMoveIfValidAndIndicateWhetherToContinue(moves, board, f, r);
             }
-        
- 
+        }
+
         return moves;
     }
 }
