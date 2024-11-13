0348651433ab8b4846c4ffcb0a5046972bae3127
kamal-hassan-1 <115949420+kamal-hassan-1@users.noreply.github.com>
2024-11-13 07:30:28 +0500

    Add files via upload

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-
 class ChessBoard {
     ChessPiece[][] pieces = new ChessPiece[8][8];
     ArrayList<ChessPiece> capturedByWhite = new ArrayList<>();
@@ -9,51 +8,52 @@ class ChessBoard {
     }
 
     boolean isAnyPieceOfSpecificColorAtPosition(ChessPosition position, char color) {
-        if(pieces[8-position.getRank()][position.getFile()-1] != null && pieces[8-position.getRank()][position.getFile()-1].color == color){
+        if(pieces[Helpers.rankToRow(position.getRank())][Helpers.fileToColumn(position.getFile())] != null && pieces[Helpers.rankToRow(position.getRank())][Helpers.fileToColumn(position.getFile())].color == color){
             return true;
         }
         return false;
     }
     boolean isThereAPiece(int rank, int file){
-        return pieces[8 - rank][file - 1] != null;
+        return pieces[Helpers.rankToRow(rank)][Helpers.fileToColumn(file)] != null;
     }
     public void fillBoardDefault(){
 
-        //Pawn pieces
+        // Pawn pieces
         for(int i=1; i<=8; i++){
-            pieces[8-2][i-1] = new PawnPiece('w',new ChessPosition(i,2));
+            pieces[Helpers.rankToRow(2)][Helpers.fileToColumn(i)] = new PawnPiece('w', new ChessPosition(i,2));
         }
         for(int i=1; i<=8; i++){
-            pieces[8-7][i-1] = new PawnPiece('b', new ChessPosition(i,7));
+            pieces[Helpers.rankToRow(7)][Helpers.fileToColumn(i)] = new PawnPiece('b', new ChessPosition(i,7));
         }
 
-        int rowForWhiteExecutivePieces = 8-1;
-        int rowForBlackExecutivePieces = 8-8;
+        int rowForWhiteExecutivePieces = Helpers.rankToRow(1);
+        int rowForBlackExecutivePieces = Helpers.rankToRow(8);
 
-        //Rock Pieces
-        pieces[rowForWhiteExecutivePieces][1-1] = new RookPiece('w',new ChessPosition(1,1));
-        pieces[rowForWhiteExecutivePieces][8-1] = new RookPiece('w',new ChessPosition(8,1));
-        pieces[rowForBlackExecutivePieces][1-1] = new RookPiece('b',new ChessPosition(1,8));
-        pieces[rowForBlackExecutivePieces][8-1] = new RookPiece('b',new ChessPosition(8,8));
+        // Rook Pieces
+        pieces[rowForWhiteExecutivePieces][Helpers.fileToColumn(1)] = new RookPiece('w', new ChessPosition(1,1));
+        pieces[rowForWhiteExecutivePieces][Helpers.fileToColumn(8)] = new RookPiece('w', new ChessPosition(8,1));
+        pieces[rowForBlackExecutivePieces][Helpers.fileToColumn(1)] = new RookPiece('b', new ChessPosition(1,8));
+        pieces[rowForBlackExecutivePieces][Helpers.fileToColumn(8)] = new RookPiece('b', new ChessPosition(8,8));
 
-        //Knight Pieces
-        pieces[rowForWhiteExecutivePieces][2-1] = new KnightPiece('w',new ChessPosition(2,1));
-        pieces[rowForWhiteExecutivePieces][7-1] = new KnightPiece('w',new ChessPosition(7,1));
-        pieces[rowForBlackExecutivePieces][2-1] = new KnightPiece('b',new ChessPosition(2,8));
-        pieces[rowForBlackExecutivePieces][7-1] = new KnightPiece('b',new ChessPosition(7,8));
+        // Knight Pieces
+        pieces[rowForWhiteExecutivePieces][Helpers.fileToColumn(2)] = new KnightPiece('w', new ChessPosition(2,1));
+        pieces[rowForWhiteExecutivePieces][Helpers.fileToColumn(7)] = new KnightPiece('w', new ChessPosition(7,1));
+        pieces[rowForBlackExecutivePieces][Helpers.fileToColumn(2)] = new KnightPiece('b', new ChessPosition(2,8));
+        pieces[rowForBlackExecutivePieces][Helpers.fileToColumn(7)] = new KnightPiece('b', new ChessPosition(7,8));
 
-        //Bishop Pieces
-        pieces[rowForWhiteExecutivePieces][3-1] = new BishopPiece('w',new ChessPosition(3,1));
-        pieces[rowForWhiteExecutivePieces][6-1] = new BishopPiece('w',new ChessPosition(6,1));
-        pieces[rowForBlackExecutivePieces][3-1] = new BishopPiece('b',new ChessPosition(3,8));
-        pieces[rowForBlackExecutivePieces][6-1] = new BishopPiece('b',new ChessPosition(6,8));
+        // Bishop Pieces
+        pieces[rowForWhiteExecutivePieces][Helpers.fileToColumn(3)] = new BishopPiece('w', new ChessPosition(3,1));
+        pieces[rowForWhiteExecutivePieces][Helpers.fileToColumn(6)] = new BishopPiece('w', new ChessPosition(6,1));
+        pieces[rowForBlackExecutivePieces][Helpers.fileToColumn(3)] = new BishopPiece('b', new ChessPosition(3,8));
+        pieces[rowForBlackExecutivePieces][Helpers.fileToColumn(6)] = new BishopPiece('b', new ChessPosition(6,8));
 
-        //King Pieces
-        pieces[rowForWhiteExecutivePieces][5-1] = new KingPiece('w', new ChessPosition(5,1));
-        pieces[rowForBlackExecutivePieces][5-1] = new KingPiece('b', new ChessPosition(5,8));
+        // King Pieces
+        pieces[rowForWhiteExecutivePieces][Helpers.fileToColumn(5)] = new KingPiece('w', new ChessPosition(5,1));
+        pieces[rowForBlackExecutivePieces][Helpers.fileToColumn(5)] = new KingPiece('b', new ChessPosition(5,8));
 
-        //Queen Pieces
-        pieces[rowForWhiteExecutivePieces][4-1] = new QueenPiece('w', new ChessPosition(4,1));
-        pieces[rowForBlackExecutivePieces][4-1] = new QueenPiece('b', new ChessPosition(4,8));
+        // Queen Pieces
+        pieces[rowForWhiteExecutivePieces][Helpers.fileToColumn(4)] = new QueenPiece('w', new ChessPosition(4,1));
+        pieces[rowForBlackExecutivePieces][Helpers.fileToColumn(4)] = new QueenPiece('b', new ChessPosition(4,8));
     }
+
 }
