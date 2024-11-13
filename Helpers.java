d5b39ac0758e734782bd7a90ab9eeddd8a30860c
kamal-hassan-1 <115949420+kamal-hassan-1@users.noreply.github.com>
2024-11-13 07:30:54 +0500

    Add files via upload

--- Helpers.java
+++ Helpers.java
@@ -2,4 +2,10 @@ public class Helpers {
     public static char invertColor(char color) {
         return ((color == 'w') ? 'b' : 'w');
     }
+    public static int rankToRow(int rank){
+        return 8-rank;
+    }
+    public static int fileToColumn(int file){
+        return file-1;
+    }
 }
