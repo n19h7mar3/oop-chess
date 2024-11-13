6ed194b2d1bb17818a52a7d88d0149899afe7239
Abdul Ahad <46815089+n19h7mar3@users.noreply.github.com>
2024-11-13 11:04:51 +0500

    Add files via upload

--- ChessGame.java
+++ ChessGame.java
@@ -2,6 +2,8 @@ public class ChessGame {
     public static void main(String[] args) {
         ChessBoard gameBoard = new ChessBoard();
         gameBoard.fillBoardDefault();
-        ChessCLI.renderBoard();
+        ChessCLI.renderBoard(gameBoard);
+       ChessCLI.getUserMove(gameBoard,'w');
+       ChessCLI.renderBoard(gameBoard);
     }
 }
