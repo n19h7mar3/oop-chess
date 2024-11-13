84286df16a7d8a1e9ca9fbe30b1483892e10f0fb
Abdul Ahad <46815089+n19h7mar3@users.noreply.github.com>
2024-11-13 07:57:22 +0500

    Add files via upload

--- ChessCLI.java
+++ ChessCLI.java
@@ -1,18 +1,63 @@
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class ChessCLI {
-    static ChessBoard board = new ChessBoard();
-    public static void renderBoard(){
-        for (int i = 0; i < 25; i++) {
-            System.out.println();
-        }
-        display();
+
+    private static final String[] chessIcons = {
+        "♚", "♛", "♜", "♝", "♞", "♟",
+        "♔", "♕", "♖", "♗", "♘", "♙"
+    };
+
+    public static void renderBoard(ChessBoard board) {
+        // Clear screen by pushing everything past the scroll view
+        for (int i = 0; i < 25; i++) System.out.println();
+
+        displayBoard(board);
+
         System.out.print("\nPieces Captured By White: ");
         System.out.println(board.capturedByWhite.toString());
         System.out.print("Pieces Captured By Black: ");
         System.out.println(board.capturedByBlack.toString());
     }
+
+    private static void displayBoard(ChessBoard board) {
+        System.out.println("      a       b       c       d       e       f       g       h");
+        System.out.println("  +-------+-------+-------+-------+-------+-------+-------+-------+");
+
+        for (int i = 0; i < 8; i++) {
+            System.out.print((i + 1) + " |");
+
+            for (int j = 0; j < 8; j++) {
+
+                if (board.pieces[i][j] == null) {
+                    System.out.print("       |");
+                    continue;
+                }
+
+                System.out.printf("   %s   |", getPieceIcon(board.pieces[i][j].pieceType, board.pieces[i][j].color));
+            }
+
+            System.out.println("\n  +-------+-------+-------+-------+-------+-------+-------+-------+");
+        }
+    }
+
+    private static String getPieceIcon(String type, char color) {
+        int index;
+        switch (type) {
+            case "king":   index = 0; break;
+            case "queen":  index = 1; break;
+            case "rook":   index = 2; break;
+            case "bishop": index = 3; break;
+            case "knight": index = 4; break;
+            case "pawn":   index = 5; break;
+
+            default:
+                throw new IllegalArgumentException("ChessCLI: Invalid pieceType specified.");
+        }
+
+        if (color == 'b') index += 6;
+        return chessIcons[index];
+    }
+
     public static void getUserMove(char color){
         Scanner sc = new Scanner(System.in);
         if(color == 'w'){
@@ -25,47 +70,5 @@ public class ChessCLI {
         char currentField = input.charAt(1);
         char nextRank = input.charAt(3);
         char nextField = input.charAt(4);
-    }
-
-    public static void display(){
-        System.out.println("\t  1\t\t  2\t\t  3\t\t  4\t\t  5\t\t  6\t\t  7\t\t  8");
-        System.out.println("   ---------------------------------------------------------------");
-
-        for (int i = 0; i < 8; i++) {
-            System.out.print((i) + " |");
-
-            for (int j = 0; j < 8; j++) {
-                ChessPiece piece = board.pieces[i][j];
-                if (piece==null) {
-                    System.out.print("   .   ");
-                } else {
-                    System.out.print("   " + convertNameToIcon(piece.pieceType, piece.color)+ "  ");
-                }
-                System.out.print("|");
-            }
-
-            System.out.println(" " + (i));
-            System.out.println("   ---------------------------------------------------------------");
-        }
-
-        System.out.println("\t  1\t\t  2\t\t  3\t\t  4\t\t  5\t\t  6\t\t  7\t\t  8");
-    }
-
-    public static String convertNameToIcon(String name, char color){
-        String[] blackIcons = {"♚","♛","♜","♝","♞","♟"};
-        String[] whiteIcons = {"♔","♕","♖","♗","♘","♙"};
-        HashMap<String, Integer> pieceToIndex = new HashMap<>();
-        pieceToIndex.put("king", 0);
-        pieceToIndex.put("queen", 1);
-        pieceToIndex.put("rook", 2);
-        pieceToIndex.put("bishop", 3);
-        pieceToIndex.put("knight", 4);
-        pieceToIndex.put("pawn", 5);
-        int index = pieceToIndex.get(name);
-        if(color == 'w'){
-            return whiteIcons[index];
-        }else{
-            return blackIcons[index];
-        }
     }
 }
